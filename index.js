193460b48421ed3aa4b34600ed8fb48e21fc9ce3
Mithun Das <mithun.das@coxautoinc.com>
2018-03-25 22:43:39 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,71 @@
+const Zbar = require('zbar');
+const speaker = require('./speaker');
+const verifier = require("./verify-purchase");
+const AWS = require('aws-sdk');
+AWS.config.update({region:'us-east-1'});
+const rekognition = new AWS.Rekognition();
+const sns = new AWS.SNS();
+const s3 = new AWS.S3();
+
+zbar = new Zbar('/dev/video1'); // connected to USB Webcam not Pi Cam
+
+
+console.log('scan your qr code');
+zbar.stdout.on('data', function(buf) {
+  console.log('data scanned : ' + buf.toString());
+  try {
+    var code = JSON.parse(buf.toString());
+    verifier.verify(code,function(err,data){
+
+        if(!err){
+            if(data){
+                speaker.speak('Congratulations ! Your ownership is verified. '+
+                'You are being watched for security reasons.');
+                console.log('+++++++++++++++++++++++++++++++++++++++++++++');
+                console.log('Congratulations ! Your ownership is verified.');
+                console.log('+++++++++++++++++++++++++++++++++++++++++++++');
+                sns.publish({
+                            Message: 'Buyer has opened your garage. http://http://192.168.0.11:8081' ,
+                            TopicArn: 'arn:aws:sns:us-east-1:027378352884:raspiFaceTextMessage'
+                          }, function (err, data) {
+                            if(err){
+
+                            }else{
+
+                            }
+
+                });
+            }else{
+              speaker.speak('You are not a verified buyer. '+
+              ' Details are sent to seller for futher action.');
+                console.log('+++++++++++++++++++++++++++++++++++++++++++++');
+                console.log('You are not a verified buyer.');
+                console.log('+++++++++++++++++++++++++++++++++++++++++++++');
+                sns.publish({
+                            Message: 'Someone trying to steal your car. http://http://192.168.0.11:8081' ,
+                            TopicArn: 'arn:aws:sns:us-east-1:027378352884:raspiFaceTextMessage'
+                          }, function (err, data) {
+                            if(err){
+
+                            }else{
+
+                            }
+
+                });
+            }
+        }
+    })
+
+  }
+  catch(error) {
+    speaker.speak('You tried to scan invalid QR Code');
+  }
+
+});
+
+//to run the program execute
+//RPC_URL="https://ngrok.io" node .
+
+zbar.stderr.on('data', function(buf) {
+    console.log(buf.toString());
+});
---
+++ verify-purchase.js
@@ -0,0 +1,28 @@
+/**
+ * Created by appstacksoultions.com on 1/24/18.
+ */
+var Web3 = require('web3');
+
+var rpcUrl = process.env.RPC_URL || 'http://localhost:8545';
+
+console.log('rpcUrl',rpcUrl);
+
+var web3 =new Web3(new Web3.providers.HttpProvider(rpcUrl));
+
+
+module.exports.verify = function(input, callback){
+
+    var  carsellContract = web3.eth.contract([{"constant":true,"inputs":[],"name":"verifyOwnership","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"buy","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[],"name":"getCar","outputs":[{"name":"","type":"string"},{"name":"","type":"string"},{"name":"","type":"string"},{"name":"","type":"uint256"},{"name":"","type":"bool"},{"name":"","type":"address"},{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"isSold","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[{"name":"make","type":"string"},{"name":"model","type":"string"},{"name":"year","type":"string"},{"name":"price","type":"uint256"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"}]);
+
+    var deployedContract = carsellContract.at(input.address);
+
+
+    deployedContract.verifyOwnership.call({from:input.owner},function(err, returnValues){
+
+        if(err){
+                callback(err);
+        }else{
+                callback(null,returnValues);
+        }
+    })
+}
